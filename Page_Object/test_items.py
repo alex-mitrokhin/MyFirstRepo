9e189281414c866d6d3b23c1f610dd494ede0a97
aleksey.mitrokhin <aleksey.mitrokhin@osinit.com>
2024-10-31 14:50:27 +0300

    add base_page.py main_page.py

--- Page_Object/test_items.py
+++ Page_Object/test_items.py
@@ -9,7 +9,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_to_basket_is_present(browser):
     browser.get(link)
-    time.sleep(1)
+    time.sleep(2)
     button = WebDriverWait(browser,10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".btn-add-to-basket"))
     )
 
